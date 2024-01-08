d44b19ee1764bb877c0ae984e7110f78a683ec40
imooxo <kimsi1127@gmail.com>
2024-01-08 16:49:36 +0900

    js class 24.01.08

--- 03_Javascript/001/js_test02.js
+++ 03_Javascript/001/js_test02.js
@@ -1,5 +1,14 @@
+console.log('====함수 만들기 1====')
 function multifly(num1, num2){
     return num1 * num2;
 }
 console.log(multifly(3,7))
 console.log(multifly(2,2))
+
+console.log('====함수 만들기 2====')
+const square = function (num) {
+    console.log(num ** 2);
+  };
+  square(4);
+  square(11);
+  square(5);
