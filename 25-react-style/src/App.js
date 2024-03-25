a08bc13f571127fcc306a099fd79cbba2441334f
imooxo <kimsi1127@gmail.com>
2024-03-25 10:10:34 +0900

    React style class + test 24.03.22

---
+++ 25-react-style/src/App.js
@@ -0,0 +1,14 @@
+import BasicCSS from "./components/BasicCss";
+import ModuleCSS from "./components/ModuleCss";
+
+function App() {
+  return (
+    <>
+      <h1>react에 style 적용하기</h1>
+      <BasicCSS/>
+      <ModuleCSS/>
+    </>
+  );
+}
+
+export default App;
