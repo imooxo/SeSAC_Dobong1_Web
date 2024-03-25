4379ceece52fd82dbeaea558fb3f4871346a2a77
imooxo <kimsi1127@gmail.com>
2024-03-25 14:47:55 +0900

    React sass&scss class + test 24.03.25

--- 25-react-style/src/App.js
+++ 25-react-style/src/App.js
@@ -1,20 +1,25 @@
 //import BasicCSS from "./components/BasicCss";
 //import ModuleCSS from "./components/ModuleCss";
-import Sass from "./components/Sass";
+//import Sass from "./components/Sass";
 //import StyledComp from "./components/StyledComp";
 //import StylePractice from "./practice/StylePractice";
+import Practice1 from "./practice/practice1";
+import Practice2 from "./practice/practice2";
 
 function App() {
   return (
     <>
-      <h1>react에 style 적용하기</h1>
-      {/* <BasicCSS/>
+      {/* <h1>react에 style 적용하기</h1>
+      <BasicCSS/>
       <ModuleCSS/>
       <StyledComp/>
 
       <h1>실습문제</h1>
       <StylePractice/> */}
-      <Sass/>
+      {/* <Sass/> */}
+      <h1>SASS 실습문제 1</h1>
+      <Practice1/>
+      <Practice2/>
     </>
   );
 }
---
+++ 25-react-style/src/practice/practice2.jsx
@@ -0,0 +1,12 @@
+import "../styles/practice2.scss";
+
+export default function Practice2(){
+    return(
+    // 컴포넌트 코드
+    <div className="circles">
+    <div className="circle pink"></div>
+    <div className="circle yellow"></div>
+    <div className="circle green"></div>
+    </div>
+    )
+}
