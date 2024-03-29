6af42509c5b459a29d5fa2a64d3601985ea26a55
imooxo <kimsi1127@gmail.com>
2024-03-29 15:11:04 +0900

    React redux class + test 24.03.27

--- 30-react-redux/src/App.js
+++ 30-react-redux/src/App.js
@@ -1,12 +1,16 @@
 import { Box1 } from "./components/Box";
-import Test from "./components/Text";
+import Bank from "./components/Practice";
+import Test from "./components/Test";
 
 function App() {
   return (
     <div className="App">
       <h1>상태관리2 redux</h1>
-      {/* <Test/> */}
-      <Box1/>
+      {/* <Test /> */}
+      <Box1 />
+
+      <hr />
+      <Bank />
     </div>
   );
 }
--- 30-react-redux/src/store/index.js
+++ 30-react-redux/src/store/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
+import { counterReducer } from "./module/counterReducer";
 import { isDataReducer } from "./module/isDataReducer";
-import { counterReducer } from "./module/conterReducer";
+import { bankReducer } from "./module/bankReducer";
 
-// 여러개의 리듀서를 하나로 합쳐주는 combineReducer
+// 여러 개의 리듀서를 하나로 합쳐주는 combineReducers
 const rootReducer = combineReducers({
-    isData:isDataReducer,
-    counter:counterReducer,
+  isData: isDataReducer,
+  counter: counterReducer,
+  bank: bankReducer,
 });
 
 export default rootReducer;
