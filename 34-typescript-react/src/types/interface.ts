161b7695f56fd5ef58658e8a6d89c2506fcb9099
imooxo <kimsi1127@gmail.com>
2024-04-05 12:11:12 +0900

    ts & React practice 24.04.05

--- 34-typescript-react/src/types/interface.ts
+++ 34-typescript-react/src/types/interface.ts
@@ -1,11 +1,20 @@
+//수업때 사용한 interface
 export interface Todo {
-    id: number;
-    text: string;
-    done: boolean;
-  }
+  id: number;
+  text: string;
+  done: boolean;
+}
 
-  export interface GTodo<T>{
-    id: number;
-    text: T;
-    done: boolean;
-  }
+export interface GTodo<T> {
+  id: number;
+  text: T;
+  done: boolean;
+}
+
+// 맛집 실습(?)에서 사용한 interface
+export interface MatzipForm {
+  imgSrc: string;
+  title: string;
+  desc: string;
+  idx: number;
+}
